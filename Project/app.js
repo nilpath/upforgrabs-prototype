7e691f36a0f2848ed3b60d05c7c2dd9f795572be
Jonatan Dahl <jd@spotify.com>
2015-04-13 00:00:13 +0200

    takePhoto and editView done with basic transition between them

--- Project/app.js
+++ Project/app.js
@@ -1,158 +1,176 @@
+var deviceWidth = 320;
+var deviceHeight = 568;
+
+/////////////////////////////////////////////////
+// Base
+/////////////////////////////////////////////////
+
 var windowLayer = new Layer({
-	x: 0, y: 0, width: 320, height: 568
+  x: 0,
+  y: 0,
+  width: deviceWidth,
+  height: deviceHeight
 });
 windowLayer.centerX();
 windowLayer.style.overflow = 'hidden';
 
-cameraImageLayer = new Layer({x: 0, y: 0, width: 320, height: 568, image:"images/lampa.png"});
-cameraImageLayer.superLayer = windowLayer;
-windowLayer.addSubLayer(cameraImageLayer);
-
-cameraImageLayer.states.animationOptions = {
-	curve: "ease-in",
-	time: 0.2
-};
-
-imageFrameLayer = new Layer({
-	x: 11, y: 90,
-	width: 298,
-	height: 298,
-	image: "images/letterBox.png"
+var takePhotoBg = new Layer({
+  x: 0,
+  y: 0,
+  width: deviceWidth,
+  height: deviceHeight,
+  image:'images/takePhotoBg.png'
 });
-imageFrameLayer.superLayer = windowLayer;
-windowLayer.addSubLayer(imageFrameLayer);
+windowLayer.addSubLayer(takePhotoBg);
 
-imageFrameLayer.states.add({
-	hide: {opacity: 0}
-});
-
-cameraImageLayer.states.add({
-	selected: {y: windowLayer.minY - imageFrameLayer.minY}
-});
-
-headline = new Layer({
-	x: 0,
-	y: 0,
-	height: 44, width: 320,
-	backgroundColor: "transparent"
-});
-headline.superLayer = windowLayer;
-headline.html = "The photo";
-headline.style.textAlign = 'center';
-headline.style.fontFamily = 'Helvetica Neue, Helvetica';
-headline.style.fontSize = '14px';
-headline.style.letterSpacing = '2px';
-headline.style.fontWeight = '300';
-headline.style.lineHeight = '44px';
-
-headline.states.add({
-	selected: {backgroundColor: "#FFF"}
-});
-
-headline.states.animationOptions = {
-	curve: "ease-in",
-	time: 0.2
-};
-
-windowLayer.addSubLayer(headline);
-
-footerLayer = new Layer({
-	x: 0, 
-	y: cameraImageLayer.maxY, 
-	width: 320, 
-	height: 204,
-	opacity: 1,
-	backgroundColor: "#FFF"
-});
-footerLayer.superLayer = windowLayer;
-windowLayer.addSubLayer(footerLayer);
-footerLayer.states.animationOptions = {
-	curve: "ease-in",
-	time: 0.2
-};
-footerLayer.centerX();
-footerLayer.states.add({
-	expand: {y: cameraImageLayer.maxY - 204, opacity: 1}
-});
-
-photoButton = new Layer({
-	y: windowLayer.maxY - 105,
-	height: 70, width: 70,
-	image: "images/takePhotoBtn.png"
-});
-photoButton.centerX();
-
-photoButton.states.add({
-	hide: {opacity: 0}
-});
-
-photoButton.on(Events.Click, function() {
-	footerLayer.states.switch("expand");
-	cameraImageLayer.states.switch("selected");
-	headline.states.switch("selected");
-	headline.html = "The sales pitch";
-	
-	photoButton.states.switchInstant("hide");
-	flashButton.states.switchInstant("hide");
-	imageFrameLayer.states.switchInstant("hide");
-	libraryButton.states.switchInstant("hide");
-	publishButton.states.switchInstant("show");
-});
-
-flashButton = new Layer({
-	y: photoButton.midY - 20,
-	x: (windowLayer.minX + 80) - 20,
-	height: 39, width: 39, 
-	image: "images/flashOffBtn.png"
-});
-
-flashButton.states.add({
-	hide: {opacity: 0}
-});
-
-libraryButton = new Layer({
-	y: photoButton.midY - 20,
-	x: (windowLayer.maxX - 80) - 20,
-	height: 39, width: 39, 
-	image: "images/chooseExistingBtn.png"
-});
-
-libraryButton.states.add({
-	hide: {opacity: 0}
-});
-
-
-publishButton = new Layer({
-	y: windowLayer.maxY - 105,
-	opacity: 0, height: 70, width: 70, image: "images/publishBtn.png"
-});
-publishButton.states.add({
-	show: {opacity: 1}
-});
-publishButton.centerX();
-
-//152 
-
-/*// Welcome to Framer
-
-// This is just demo code. Feel free to delete it all.
-
-imageLayer = new Layer({x:0, y:0, width:128, height:128, image:"imagesicon.png"})
-imageLayer.center()
-
-// Define a set of states with names (the original state is 'default')
-imageLayer.states.add({
-	second: {y:100, scale:0.6, rotationZ:100},
-	third:  {y:300, scale:1.3},
-	fourth:	{y:200, scale:0.9, rotationZ:200},
-})
-
-// Set the default animation options
-imageLayer.states.animationOptions = {
-	curve: "spring(500,12,0)"
+Framer.Defaults.Animation = {
+  time: 0.3,
+  curve: 'bezier-curve([0, 0, .1, 1])'
 }
 
-// On a click, go to the next state
-imageLayer.on(Events.Click, function() {
-	imageLayer.states.next()
-})*/
+/////////////////////////////////////////////////
+// Header
+/////////////////////////////////////////////////
+
+var headerHeight = 70;
+var header = new Layer({
+  x: 0,
+  y: 0,
+  width: deviceWidth,
+  height: headerHeight,
+  backgroundColor: 'transparent'
+});
+windowLayer.addSubLayer(header);
+
+var cancelBtn = new Layer({
+  x: 25,
+  y: headerHeight/2-17/2,
+  width: 17,
+  height: 17,
+  image:'images/cancelBtn.png'
+});
+header.addSubLayer(cancelBtn);
+
+var publishBtn = new Layer({
+  x: deviceWidth-25-35,
+  y: headerHeight/2-35/2,
+  width: 35,
+  height: 35,
+  image:'images/publishBtn.png',
+  opacity: 0,
+});
+publishBtn.states.add({
+  visible: {
+    opacity: 1
+  }
+})
+header.addSubLayer(publishBtn);
+
+/////////////////////////////////////////////////
+// Take photo footer
+/////////////////////////////////////////////////
+
+/// Definitions
+
+takePhotoFooterHeight = 177;
+var takePhotoFooter = new Layer({
+  x: 0,
+  y: windowLayer.maxY-takePhotoFooterHeight,
+  width: deviceWidth,
+  height: takePhotoFooterHeight,
+  backgroundColor: 'transparent'
+});
+windowLayer.addSubLayer(takePhotoFooter);
+
+var takePhotoBtn = new Layer({
+  y: takePhotoFooterHeight/2-80/2,
+  width: 80,
+  height: 80,
+  image:'images/takePhotoBtn.png'
+});
+takePhotoBtn.centerX();
+takePhotoFooter.addSubLayer(takePhotoBtn);
+
+var flashOffBtn = new Layer({
+  y: takePhotoFooterHeight/2-40/2,
+  x: 55,
+  width: 40,
+  height: 40,
+  image:'images/flashOffBtn.png'
+});
+takePhotoFooter.addSubLayer(flashOffBtn);
+
+var chooseExistingBtn = new Layer({
+  y: takePhotoFooterHeight/2-40/2,
+  x: deviceWidth-55-40,
+  width: 40,
+  height: 40,
+  image:'images/chooseExistingBtn.png'
+});
+takePhotoFooter.addSubLayer(chooseExistingBtn);
+
+////////////////////////////////////////////////
+// Edit photo
+/////////////////////////////////////////////////
+
+var editPhotoFooterHeight = 248;
+var editPhotoFooter = new Layer({
+  y: windowLayer.maxY,
+  x: 0,
+  width: deviceWidth,
+  height: editPhotoFooterHeight
+});
+windowLayer.addSubLayer(editPhotoFooter);
+
+var descriptionEmpty = new Layer({
+  y: 0,
+  x: 0,
+  width: deviceWidth,
+  height: 68,
+  image:'images/descriptionEmpty.png'
+});
+editPhotoFooter.addSubLayer(descriptionEmpty);
+
+var map = new Layer({
+  y: editPhotoFooterHeight-148,
+  x: 0,
+  width: deviceWidth,
+  height: 148,
+  image:'images/map.png'
+});
+editPhotoFooter.addSubLayer(map);
+
+var locationBarEmpty = new Layer({
+  y: descriptionEmpty.height,
+  x: 0,
+  width: deviceWidth,
+  height: 45,
+  image:'images/locationBarEmpty.png'
+});
+editPhotoFooter.addSubLayer(locationBarEmpty);
+
+/////////////////////////////////////////////////
+// States
+/////////////////////////////////////////////////
+
+takePhotoBg.states.add({
+  editMode: {
+    y: -header.maxY
+  }
+})
+
+editPhotoFooter.states.add({
+  visible: {
+    y: windowLayer.maxY-editPhotoFooterHeight
+  }
+})
+
+/////////////////////////////////////////////////
+// Behaviour
+/////////////////////////////////////////////////
+
+takePhotoBtn.on(Events.Click, function() {
+  publishBtn.states.next();
+  takePhotoBg.states.next();
+  editPhotoFooter.states.next();
+})
